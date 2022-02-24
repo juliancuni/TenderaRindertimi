c78baf2423b1426c0af6661306c4e7bdb4d526a3
Julian Cuni <julian.cuni@microservices.al>
2022-02-24 03:30:57 +0100

    Prettier, Eslint, MUI

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,30 @@
+<h1>Hello {name}!</h1>
+
+<Checkbox bind:checked>Checkbox</Checkbox>
+
+<p>Checkbox is <strong>{checked ? 'checked' : 'unchecked'}</strong></p>
+
+<Button
+    outlined
+    shaped
+    color="Red"
+    on:click={() => { checked = !checked }}
+>
+    Inverse
+</Button>
+
 <script>
-	export let name;
+    export let name;
+    // optional import focus-visible polyfill only once
+    import 'focus-visible';
+    // import any components
+    import { Button, Checkbox } from 'svelte-mui';
+
+    let checked = true;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    h1 {
+        color: purple;
+    }
 </style>
