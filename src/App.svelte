6a52fd7cb23ac4716fb4c9a47b0831be5bd4fc8d
Julian Cuni <julian.cuni@microservices.al>
2022-02-24 03:35:39 +0100

    Convert to TS

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+  export let name: string;
+  // optional import focus-visible polyfill only once
+  import 'focus-visible';
+  // import any components
+  import {Button, Checkbox} from 'svelte-mui';
+
+  let checked = true;
+</script>
+
 <h1>Hello {name}!</h1>
 
 <Checkbox bind:checked>Checkbox</Checkbox>
@@ -5,26 +15,18 @@
 <p>Checkbox is <strong>{checked ? 'checked' : 'unchecked'}</strong></p>
 
 <Button
-    outlined
-    shaped
-    color="Red"
-    on:click={() => { checked = !checked }}
+  outlined
+  shaped
+  color="Red"
+  on:click={() => {
+    checked = !checked;
+  }}
 >
-    Inverse
+  Inverse
 </Button>
 
-<script>
-    export let name;
-    // optional import focus-visible polyfill only once
-    import 'focus-visible';
-    // import any components
-    import { Button, Checkbox } from 'svelte-mui';
-
-    let checked = true;
-</script>
-
 <style>
-    h1 {
-        color: purple;
-    }
+  h1 {
+    color: purple;
+  }
 </style>
